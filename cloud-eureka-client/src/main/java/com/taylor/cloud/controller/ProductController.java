6eaa358635362f985fac8a2ac9eb024527b8a17c
tdytaylor <949722633@qq.com>
2019-01-29 14:49:47 +0800

    更改项目名

--- cloud-eureka-client/src/main/java/com/taylor/cloud/controller/ProductController.java
+++ cloud-eureka-client/src/main/java/com/taylor/cloud/controller/ProductController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/product")
+@RequestMapping("/api/v1/product/")
 public class ProductController {
 
   @Autowired
