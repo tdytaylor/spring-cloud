472e2c74871b7b52cdcde6d4b4bb6ca09499a4ec
tdytaylor <949722633@qq.com>
2019-01-29 14:29:17 +0800

    新增spring boot项目

---
+++ cloud-spring-boot/src/main/java/com/taylor/cloud/CloudSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.taylor.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudSpringBootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CloudSpringBootApplication.class, args);
+  }
+}
