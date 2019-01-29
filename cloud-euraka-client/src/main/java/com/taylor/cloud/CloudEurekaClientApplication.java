f027fddc2ce4afd07e3013eba898977e1439b6f3
tdytaylor <949722633@qq.com>
2019-01-29 11:12:20 +0800

    eureka-client项目提交

---
+++ cloud-euraka-client/src/main/java/com/taylor/cloud/CloudEurekaClientApplication.java
@@ -0,0 +1,15 @@
+package com.taylor.cloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CloudEurekaClientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CloudEurekaClientApplication.class, args);
+  }
+}
